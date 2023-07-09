854a0e8984772edc6169e4fff65b342fabe6b5bc
arthurcaraujo <arthur-lagamar@hotmail.com>
2023-07-09 08:21:50 -0300

    Setting up Open Graph protocol

---
+++ src/components/Header.js
@@ -0,0 +1,5 @@
+export default function Header() {
+    return (
+        <main>Hello Header!</main>
+    )
+}
---
+++ src/components/Main.js
@@ -0,0 +1,5 @@
+export default function Main() {
+    return (
+        <main>Hello Main!</main>
+    )
+}
