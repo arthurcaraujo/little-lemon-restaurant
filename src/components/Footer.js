06bcd0f801dbb356ce39ebd3d9e2c4d115e2eb6e
arthurcaraujo <arthur-lagamar@hotmail.com>
2023-07-10 14:01:39 -0300

    Styling elements

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,68 @@
+import "./Footer.css";
+import logoFooter from "../assets/logo-footer.png";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faLocationDot, faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook, faInstagram, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
+
+const contactInfo = [
+    { icon: faPhone, id: "phone", info: "(012) 345-6789" },
+    { icon: faEnvelope, id: "email", info: <a href="mailto:contact@littlelemon.com">contact@littlelemon.com</a> },
+    { icon: faLocationDot, id: "location", info: "151w W Adams St, Chicago, IL 60603" }
+];
+
+const socialMedia = [
+    { icon: faFacebook, name: "facebook" },
+    { icon: faInstagram, name: "instagram" },
+    { icon: faTwitter, name: "twitter" },
+    { icon: faYoutube, name: "youtube" }
+];
+
 export default function Footer() {
     return (
-        <main>Hello Footer!</main>
+        <footer>
+            <section id="footer-logo">
+                <img alt="Little Lemon logo" src={logoFooter} />
+            </section>
+
+            <section id="sitemap">
+                <h2>Sitemap</h2>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Menu</li>
+                    <li>Reservations</li>
+                    <li>Order Online</li>
+                    <li>Login</li>
+                </ul>
+            </section>
+
+            <section id="contact-info">
+                <h2>Contact us</h2>
+                <address>
+                    {contactInfo.map(contact =>
+                        <p key={contact.id}>
+                            <FontAwesomeIcon icon={contact.icon} size="lg" /> {contact.info}
+                        </p>
+                    )}
+                </address>
+            </section>
+
+            <section id="social-media">
+               <h2>Our social media</h2>
+                <ul>
+                    {socialMedia.map(social =>
+                        <li key={social.name}>
+                            <a
+                                href={`https://www.${social.name}.com`}
+                                rel="noreferrer"
+                                target="_blank"
+                            >
+                                <FontAwesomeIcon icon={social.icon} size="lg" />
+                            </a>
+                        </li>
+                    )}
+                </ul>
+            </section>
+        </footer>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,38 @@
+import "./Header.css";
+import { useState } from "react";
+import logo from "../assets/logo.png";
+import hamburgerMenuIcon from "../assets/icon-hamburgermenu.svg";
+
 export default function Header() {
+    const [expandNavbar, setExpandNavbar] = useState(false);
+
     return (
-        <main>Hello Header!</main>
+        <header>
+            <nav>
+                <a id="navbar-logo">
+                    <img alt="Little Lemon logo" src={logo} />
+                </a>
+                <img
+                    alt="Hamburger menu icon"
+                    id="hamburger-icon"
+                    onClick={() => setExpandNavbar(!expandNavbar)}
+                    src={hamburgerMenuIcon}
+                />
+                <ul
+                    className={expandNavbar
+                        ? "navbar-links expanded"
+                        : "navbar-links"
+                    }
+                    onClick={() => setExpandNavbar(!expandNavbar)}
+                >
+                    <li><a>Home</a></li>
+                    <li><a>About</a></li>
+                    <li><a>Menu</a></li>
+                    <li><a>Reservations</a></li>
+                    <li><a>Order Online</a></li>
+                    <li><a>Login</a></li>
+                </ul>
+            </nav>
+        </header>
     )
 }
